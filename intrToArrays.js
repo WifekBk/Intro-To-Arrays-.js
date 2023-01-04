cadecf4c99f25bdf33f00519d6c970ffc5aae635
Wifek Benkhoud <wifekbenkhoud@gmail.com>
2023-01-04 21:14:28 +0100

    ex

--- intrToArrays.js
+++ intrToArrays.js
@@ -1,1 +1,75 @@
-f
+var myArray[ , , ]; // q1
+[0]
+[2]
+[3]
+
+
+var Ar=[1, 2, 3, 4, 5, 6, 7, 'The quick brown fox jumped over  the lazy dog', true, false, true ] //q3
+
+function emptyArray(){
+    var emptyArray=[];
+     return emptyArray
+ }
+
+
+
+
+ function numbersArray() {
+    var  numbersArray=[1, 2, 3, 4, 5];
+     return numbersArray
+    } // q5
+    
+  function booleansArray() {
+   var boo=[true, true, true]
+    return boo;
+}  
+
+
+function stringsArray() {
+    var name=["benkhoud","wifek"] 
+     return name
+ }    //q7
+
+
+
+
+ function arrayLength(array) {
+    return array.length 
+  }    //q8
+
+  /// more practice
+
+  function firstElement(array) {
+    return array[0] 
+  }  
+
+
+  function lastElement(array) {
+    return array[array.length-1]
+ }  
+
+
+ function getNthElement(array, number) {
+    return array[number];
+   }
+
+
+   function push(array, element) {
+    var items= array;
+      items.push(element);
+      return items
+  }  
+  
+  function pop(array) {
+    return array.pop ()
+ } 
+
+
+
+
+
+
+
+
+
+
