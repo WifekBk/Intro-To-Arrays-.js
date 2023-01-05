752edc690e807c56c37f94bce7556fcc3b0f9958
Wifek Benkhoud <wifekbenkhoud@gmail.com>
2023-01-05 20:15:59 +0100

    to advanced 9

--- intrToArrays.js
+++ intrToArrays.js
@@ -1,7 +1,8 @@
 var myArray[ , , ]; // q1
-[0]
-[2]
-[3]
+var array['dog', 'cat', 'fox', 'monkey']
+array[0]//'dog'
+array[2]//'fox'
+array[3]//'monkey'
 
 
 var Ar=[1, 2, 3, 4, 5, 6, 7, 'The quick brown fox jumped over  the lazy dog', true, false, true ] //q3
@@ -55,14 +56,61 @@ function stringsArray() {
 
 
    function push(array, element) {
-    var items= array;
-      items.push(element);
-      return items
-  }  
+    array[array.length]= element
+        return array
+    }  
   
-  function pop(array) {
-    return array.pop ()
- } 
+    function pop(array) {
+      var res=[]
+       res= [array[0]]
+       for (i=1; i<array.length-1;i++){
+           res[i]=array[i];
+       }
+   return res;
+       return res;
+   }  
+
+
+   function pop(array) {
+    array.splice(array.length-1,1)
+     return array
+ }  
+
+
+
+   function unshift(array, element) {
+    var tab=[];
+     tab[0]= element;
+    for (i=1; i<array.length+1; i++){
+        tab[i]= array[i-1]
+    }
+     return tab
+ }  
+
+ function unshift(array, element) {
+  array[array.length]= element;
+    return array
+}  
+
+
+
+
+
+ function shift(array){
+  array.splice(0,1);
+  return array
+}
+
+
+function reassignLast(array, element) {
+  array[array.length-1]= element;
+   return array
+}  
+
+function reassignNthElement(array, index, element) {
+  array[index]= element;
+   return array
+}  
 
 
 
